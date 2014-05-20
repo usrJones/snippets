ce5ea3cb162d00f9ee9751fd3eddf51e3e66678f
usrJones <>
2014-05-20 12:45:28 +0300

    20.5.2014

--- public_html/snippets/js/clock.js
+++ public_html/snippets/js/clock.js
@@ -1,7 +1,32 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+function haeAika() {
+    var aika = new Date();
+    var tunnit = aika.getHours();
+    var minuutit = aika.getMinutes();
+    var sekunnit = aika.getSeconds();
+    var sadasosat = aika.getMilliseconds() / 10;
+    
+    sadasosat = Math.round(sadasosat);
+    
+    if (tunnit < 10) {
+        tunnit = "0" + tunnit;
+    }
+    
+    if (minuutit < 10) {
+        minuutit = "0" + minuutit;
+    }
+    
+    if (sekunnit < 10) {
+        sekunnit = "0" + sekunnit;
+    }
+    
+    if (sadasosat == 100) {
+        sadasosat = "00";
+    } else if (sadasosat < 10) {
+        sadasosat = "0" + sadasosat;
+    }
+    
+    document.getElementById('kello').innerHTML = 
+            tunnit + ":" + minuutit + ":" + 
+            sekunnit + ":" + sadasosat;
+    ajastin = setTimeout(function(){  haeAika()  }, 80);
+}
--- public_html/snippets/js/slider.js
+++ public_html/snippets/js/slider.js
@@ -1,7 +1,11 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+function scrollHere(id){
+    $('html, body').animate({ //.animate -toiminto koko dokumenttiin
+    scrollTop: $("#" + id).offset() .top},		'slow'); //scrollataan tietyn id:n yläreunaan.
+}
 
-
+$ (function(){
+    $('.slide').css({'height':(( //slide -classin korkeus tyylipohjassa
+        $(window).height() //asetetaan korkeudeksi selainikkunan korkeus
+        )-100) + 'px' //miinus 100px, joka jostain syystä valuu yli. Todennäköisesti jotain default padding/margin puppua selaimen työkalupalkin lisäksi.
+    });
+});
